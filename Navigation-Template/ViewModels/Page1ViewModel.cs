120a6bcc4ca2008884ccd3e442485da21cc38891
Eduardo-Gonzalez-eng <eduardo.trabajos12@gmail.com>
2023-12-12 11:03:53 -0300

    se agregaron los archivos a las carpetas

---
+++ Navigation-Template/ViewModels/Page1ViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Navigation_Template.ViewModels
+{
+    internal class Page1ViewModel
+    {
+    }
+}
